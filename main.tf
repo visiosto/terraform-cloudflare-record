24034b0187c9ee9b70c9fa4529ee27fdd7bfab14
Antti Kivi <antti@anttikivi.fi>
2025-04-21 13:22:22 +0300

    fix: try to use the `zone_id` variable for the Cloudflare zone ID

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "cloudflare_dns_record" "this" {
   name     = var.name
   ttl      = var.ttl
   type     = var.type
-  zone_id  = data.cloudflare_zone.current.id
+  zone_id  = data.cloudflare_zone.current.zone_id
   content  = var.content
   priority = var.type == "MX" ? var.priority : null
   proxied  = var.proxied

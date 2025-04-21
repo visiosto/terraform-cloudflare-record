9361bc528a748a437fc5ab7197fe7af991c7bf3a
Antti Kivi <antti@anttikivi.fi>
2025-04-21 10:25:59 +0300

    feat!: remove automatic quoting of the `TXT` records

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to
 [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [unreleased]
+
+### Removed
+
+- Automatic quoting of `TXT` records.
+
 ## [0.1.1] - 2025-04-20
 
 ### Fixed
--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "cloudflare_dns_record" "this" {
   ttl      = var.ttl
   type     = var.type
   zone_id  = data.cloudflare_zone.current.id
-  content  = var.type == "TXT" && !startswith(var.content, "\"") && !endswith(var.content, "\"") ? format("\"%s\"", var.content) : var.content
+  content  = var.content
   priority = var.type == "MX" ? var.priority : null
   proxied  = var.proxied
 }

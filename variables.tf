357bc0527551394c8edb44339ca3d4de5b8b0cd0
Antti Kivi <antti@anttikivi.fi>
2025-04-22 12:21:31 +0300

    feat: validate priority for MX records

--- variables.tf
+++ variables.tf
@@ -6,6 +6,7 @@ variable "content" {
 variable "name" {
   description = "The name of the DNS record to create."
   type        = string
+
   validation {
     condition     = length(var.name) > 0
     error_message = "The name of the DNS record cannot be empty."
@@ -16,6 +17,11 @@ variable "priority" {
   description = "The priority of the DNS record, if it is an MX record."
   type        = number
   default     = 0
+
+  validation {
+    condition     = var.type != "MX" || var.priority > 0
+    error_message = "Priority must be greater than 0 for MX records."
+  }
 }
 
 variable "proxied" {
@@ -28,6 +34,7 @@ variable "ttl" {
   description = "The time to live (TTL) for the DNS record."
   type        = number
   default     = 1
+
   validation {
     condition     = var.ttl >= 1 && var.ttl <= 86400
     error_message = "TTL must be between 1 and 86400 seconds."
@@ -37,6 +44,7 @@ variable "ttl" {
 variable "type" {
   description = "The type of the DNS record."
   type        = string
+
   validation {
     condition     = contains(["A", "AAAA", "CNAME", "TXT", "MX"], var.type)
     error_message = "Invalid DNS record type specified."
